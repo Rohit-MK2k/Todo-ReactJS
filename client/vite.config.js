1321b7fdb588d2825df5cf7739844a524b3d1815
Rohit Malakar <92802090+Rohit-MK2k@users.noreply.github.com>
2024-08-12 18:53:12 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api':{
-        target: 'https://todo-react-js-server.vercel.app/',
+        target: `${window.location.origin}`,
         changeOrigin: true,
       }
     }
