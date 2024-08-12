cb938c2adbf64e892a9b27681b4af62d17d08398
Rohit Malakar <92802090+Rohit-MK2k@users.noreply.github.com>
2024-08-12 18:04:22 +0530

    deploy ready server.js

--- server/server.js
+++ server/server.js
@@ -30,9 +30,9 @@ app.use("/api/todo",protect, userList)
 const __dirname1 = 'https://todo-react-js-beryl.vercel.app/'
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname1, "client/dist")))
+    app.use(express.static(path.join(__dirname1, "/dist")))
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname1,"client","dist","index.html"))   
+        res.sendFile(path.resolve(__dirname1,"dist","index.html"))   
     })
 } else {
     app.get("/", (req, res) => {
