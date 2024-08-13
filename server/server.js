bf0ca327b002ff8ff52b24f41ca6148899475d9d
Rohit <rohitmalakar007@gmail.com>
2024-08-14 00:23:16 +0530

    small changes

--- server/server.js
+++ server/server.js
@@ -26,20 +26,22 @@ app.use("/api/todo",protect, userList)
 
 // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve()
+// const __dirname1 = path.resolve()
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname1, "/client/build")))
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname1,"client","build","index.html"))   
-    })
-} else {
-    app.get("/", (req, res) => {
-        res.send("API is running ")
-    })
-}
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname1, "/client/build")))
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname1,"client","build","index.html"))   
+//     })
+// } else {
+//     app.get("/", (req, res) => {
+//         res.send("API is running ")
+//     })
+// }
 
-
+app.get("/", (req, res) => {
+    res.send("API is running ")
+})
 // --------------------------deployment------------------------------
 
 
