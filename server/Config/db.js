327f6f54a940f899d341ca107e64039793c0067a
Rohit <rohitmalakar007@gmail.com>
2024-08-14 00:12:53 +0530

    small changes

--- server/Config/db.js
+++ server/Config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
+        console.log(`MongoDB Connected`)
     } catch (err) {
         console.log(err)
         process.exit(1)
