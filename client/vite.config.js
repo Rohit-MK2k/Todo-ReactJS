f31afc11da47b934556b46fb033619618d461863
Rohit <rohitmalakar007@gmail.com>
2024-08-19 21:48:53 +0530

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv/config'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +10,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api':{
-        target: `${window.location.origin}`,
+        target: "import.meta.env.PRODUCTION_SERVER",
         changeOrigin: true,
       }
     }
