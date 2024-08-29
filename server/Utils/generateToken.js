a3c80e7eded6730f6dba7a2481b73c3d50ce5b1f
Rohit Malakar <92802090+Rohit-MK2k@users.noreply.github.com>
2024-08-29 17:55:14 +0530

    Update generateToken.js

--- server/Utils/generateToken.js
+++ server/Utils/generateToken.js
@@ -9,7 +9,7 @@ const generateToken = (res, id) => {
     res.cookie('jwt', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: 30 * 24 * 60 * 60 * 1000,
     })
 }
